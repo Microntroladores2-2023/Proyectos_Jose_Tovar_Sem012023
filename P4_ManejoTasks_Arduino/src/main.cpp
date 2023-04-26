24161e8d63b5d5f5a4ed991630436979d51ae17b
alejandrotovar9 <jatovar02@gmail.com>
2023-04-25 23:26:10 -0400

    commit

--- P4_ManejoTasks_Arduino/src/main.cpp
+++ P4_ManejoTasks_Arduino/src/main.cpp
@@ -4,7 +4,6 @@ TaskHandle_t xHandle;
 
 void setup()
 {
-
   xTaskCreatePinnedToCore(Boton, "Boton", 1024 * 2, NULL, 1, NULL, 0);
   xTaskCreatePinnedToCore(Blink, "Parpadeo", 1024 * 2, NULL, 1, &xHandle, 0);
 }
