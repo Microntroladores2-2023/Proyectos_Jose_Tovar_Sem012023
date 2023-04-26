b5a4af8792670e55ebba1e701b50177952cc7e0e
alejandrotovar9 <jatovar02@gmail.com>
2023-04-25 23:47:08 -0400

    cambios

--- P5_PruebaADC/src/main.c
+++ P5_PruebaADC/src/main.c
@@ -12,36 +12,37 @@ void TareaADC(void *pvParameters) // Esta es una tarea
 {
 #define numeroMuestras 128
 
-    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
+    //Se fija la atenuacion y voltaje de referencia
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 0, &adc1_chars);
 
-    adc1_config_width(ADC_WIDTH_BIT_DEFAULT);
+    adc1_config_width(ADC_WIDTH_BIT_12);
     adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_11);
 
     int muestras[numeroMuestras], promedio;
     int i = 0;
 
-    int adc_value = adc1_get_raw(ADC1_CHANNEL_4);
+    int adc_value = adc1_get_raw(ADC1_CHANNEL_4); //Se toma medicion de valor en canal 4
     for (int j = 0; j < numeroMuestras; j++)
         muestras[j] = adc_value;
 
     uint32_t voltage;
 
-    while (1)
+    //Loop infnito de la tarea
+    while (1) 
     {
         adc_value = adc1_get_raw(ADC1_CHANNEL_4);
-
-        printf("ADC Value raw: %d \n", adc_value);
+        printf("ADC Value raw: %d \n", adc_value); //Se imprime valor obtenido
         muestras[i++] = adc_value;
         promedio = 0;
         for (int j = 0; j < numeroMuestras; j++)
-            promedio = promedio + muestras[j];
+            promedio = promedio + muestras[j]; //Se calcula promedio con muestras obtenidas
 
         printf("ADC Value promedio: %d \n", promedio / numeroMuestras);
 
         if (i >= numeroMuestras)
             i = 0;
 
-        voltage = esp_adc_cal_raw_to_voltage(adc_value, &adc1_chars);
+        voltage = esp_adc_cal_raw_to_voltage(adc_value, &adc1_chars); //Se convierte a voltaje
         printf("Voltage: %d mV",(int)voltage);
         printf("\n");
         vTaskDelay(500 / portTICK_PERIOD_MS);
